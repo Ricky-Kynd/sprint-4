896a64f2af062b0cab1909f9bdf74e3e72db7529
Ricky-Kynd <Ricky-Kynd@gmail.com>
2016-04-11 09:05:24 +1200

    Invert regexp

--- invert-regular-expression-matches.js
+++ invert-regular-expression-matches.js
@@ -1,1 +1,11 @@
+// Setup
+var testString = "How many non-space characters are there in this sentence?";
 
+// Only change code below this line.
+
+var expression = /\S/g;  // Okay? now what?
+
+// Only change code above this line
+
+// This code counts the matches of expression in testString
+var nonSpaceCount = testString.match(expression).length;
